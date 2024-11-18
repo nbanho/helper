abb36c5400cbc5c4aa52d6c644f61cbb03c430f9
nbanho <nicbanjo@gmail.com>
2024-11-18 17:06:25 +0100

    Pvalue formatting

--- R/formatting.R
+++ R/formatting.R
@@ -11,3 +11,22 @@
 round_k <- function(x, k = 2) {
   trimws(format(round(x, k), nsmall = k))
 }
+
+
+#' Format p-values
+#'
+#' @description Format p-values in typical journal reporting syle.
+#'
+#' @param x numeric p-value
+#'
+#' @returned formatted p-value (character)
+#'
+
+format_pvalue <- function(x) {
+  ifelse(
+    x < .001, "p < 0.001",
+    ifelse(x < 0.01, paste("p =", round(x, 3)),
+      paste("p =", round(x, 2))
+    )
+  )
+}
